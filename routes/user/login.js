f1b4d343a3688ce5b96f9d3fd4c01760b052d838
Shreyas Pahune <pahuneshreyas@gmail.com>
2023-01-01 15:39:08 +0530

    refactor: updated folder struct

--- routes/user/login.js
+++ routes/user/login.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userModel = require('../model/user.model')
+const userModel = require('../../model/user.model')
 const bcrypt = require("bcryptjs");
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
--- routes/user/register.js
+++ routes/user/register.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userModel = require('../model/user.model')
+const userModel = require('../../model/user.model')
 const bcrypt = require("bcryptjs");
 router.post("/register", async (req, res) => {
     try {
